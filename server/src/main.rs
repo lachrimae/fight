2f0b4a627da4e53f560125ca01dc67abc3a71106
Curran McConnell <curran.mcconnell@protonmail.com>
2023-08-14 13:37:28 -0400

    start writing data types

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize)]
+enum GameState {
+    Lobbied,
+    Started,
+    Completed,
+    Cancelled,
+}
+
+#[derive(Serialize)]
+struct Game {
+    id: String,
+    state: GameState,
+}
+
+#[derive(Serialize)]
+struct GameJoinInfo {
+    mac_key: String,
+    game: Game,
+}
+
+#[tokio::main]
+async fn main() {}

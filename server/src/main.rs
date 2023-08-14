cdde63f5c3fa288760762fe30bbc812af8e3b200
Curran McConnell <curran.mcconnell@protonmail.com>
2023-08-14 13:54:34 -0400

    initialize app config

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,14 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{delete, get, post},
+    Json, Router,
+};
+use deadpool_postgres;
+use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use tokio_postgres::NoTls;
 
 #[derive(Serialize)]
 enum GameState {
@@ -20,5 +30,33 @@ struct GameJoinInfo {
     game: Game,
 }
 
+#[derive(Debug, Deserialize)]
+struct Config {
+    pg: deadpool_postgres::Config,
+}
+
+impl Config {
+    pub fn from_env() -> Result<Self, ::config::ConfigError> {
+        let mut cfg: Config = ::config::Config::builder()
+            .add_source(config::Environment::default().separator("__"))
+            .build()?
+            .try_deserialize()?;
+        cfg.pg.manager = Some(deadpool_postgres::ManagerConfig {
+            recycling_method: deadpool_postgres::RecyclingMethod::Fast,
+        });
+        Ok(cfg)
+    }
+}
+
+struct App {
+    db_pool: deadpool_postgres::Pool,
+}
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let mut cfg = Config::from_env().unwrap();
+    let pool = cfg
+        .pg
+        .create_pool(Some(deadpool_postgres::Runtime::Tokio1), NoTls)
+        .unwrap();
+}
